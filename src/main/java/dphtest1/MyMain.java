b5cc396d8a807f3cd7b12e0f5212e7792307d48e
ashburndev <ashburndev@gmail.com>
2021-03-18 09:35:50 -0400

    first commit

---
+++ src/main/java/dphtest1/MyMain.java
@@ -0,0 +1,13 @@
+package dphtest1;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class MyMain {
+
+    public static void main(String[] args) {
+        Document document;
+        Element element;
+        System.out.println("hello there maven");
+    }
+}
